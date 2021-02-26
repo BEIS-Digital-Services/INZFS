c0f10573689dfd5af95ab70c53b8c10fc4d3c7f6
Lorenzo Lane <lorenzo.lane@beis.gov.uk>
2021-02-26 16:19:15 +0000

    Added MVC functionality

---
+++ INZFS.MVC/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "INZFS.MVC",
+    Author = "Lorenzo Lane",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "INZFS.MVC",
+    Category = "Content Management"
+)]
---
+++ INZFS.MVC/Startup.cs
@@ -0,0 +1,33 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+namespace INZFS.MVC
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            routes.MapAreaControllerRoute(
+                name: "BlogPost",
+                areaName: "INZFS.MVC",
+                pattern: "BlogPost/Index",
+                defaults: new { controller = "BlogPost", action = "Index" }
+            );
+            routes.MapAreaControllerRoute(
+                name: "FundApplication",
+                areaName: "INZFS.MVC",
+                pattern: "FundApplication/Index",
+                defaults: new { controller = "FundApplication", action = "Index" }
+
+                );
+            
+        }
+    }
+}
