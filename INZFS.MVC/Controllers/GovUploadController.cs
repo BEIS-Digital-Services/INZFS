da659a19fd2d367de017b20634d11eaf15b27bb8
Lorenzo Lane <lorenzo.lane@beis.gov.uk>
2021-03-24 16:22:32 +0000

    Added dummy upload controller

---
+++ INZFS.MVC/Controllers/GovUploadController.cs
@@ -0,0 +1,93 @@
+﻿using INZFS.MVC.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
+using OrchardCore.DisplayManagement.Notify;
+using OrchardCore.FileStorage;
+using OrchardCore.Media;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INZFS.MVC.Controllers
+{
+    public class GovUploadController : Controller
+    {
+        private const string TestFileRelativePath = "GovUploadTest/TestFile.txt";
+
+        private const string UploadedFileFolderRelativePath = "GovUpload/UploadedFiles";
+
+        private readonly IMediaFileStore _mediaFileStore;
+        private readonly INotifier _notifier;
+        private readonly IHtmlLocalizer H;
+        private readonly IGovFileStore _govFileStore;
+
+        public GovUploadController(
+            IMediaFileStore mediaFileStore,
+            INotifier notifier,
+            IHtmlLocalizer<GovUploadController> htmlLocalizer,
+            IGovFileStore govFileStore)
+        {
+            _mediaFileStore = mediaFileStore;
+            _notifier = notifier;
+            _govFileStore = govFileStore;
+            H = htmlLocalizer;
+        }
+
+        public async Task<string> CreateFileInMediaFolder()
+        {
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes("Hello world!")))
+            {
+                await _mediaFileStore.CreateFileFromStreamAsync(TestFileRelativePath, stream, true);
+            }
+
+            var fileInfo = await _mediaFileStore.GetFileInfoAsync(TestFileRelativePath);
+
+            var publicUrl = _mediaFileStore.MapPathToPublicUrl(TestFileRelativePath);
+
+            return $"Successfully created file! File size: {fileInfo.Length} bytes. Public URL: {publicUrl}";
+        }
+        public async Task<string> ReadFileFromMediaFolder()
+        {
+        
+            if (await _mediaFileStore.GetFileInfoAsync(TestFileRelativePath) == null)
+            {
+                return "Create the file first!";
+            }
+
+            using var stream = await _mediaFileStore.GetFileStreamAsync(TestFileRelativePath);
+            using var streamReader = new StreamReader(stream);
+            var content = await streamReader.ReadToEndAsync();
+
+
+            return $"File content: {content}";
+        }
+        public ActionResult UploadFileToMedia() => View();
+
+        [HttpPost, ActionName(nameof(UploadFileToMedia)), ValidateAntiForgeryToken]
+        public async Task<ActionResult> UploadFileToMediaPost(IFormFile file)
+        {
+            var mediaFilePath = _mediaFileStore.Combine(UploadedFileFolderRelativePath, file.FileName);
+
+            using (var stream = file.OpenReadStream())
+            {
+                await _mediaFileStore.CreateFileFromStreamAsync(mediaFilePath, stream);
+            }
+
+            _notifier.Information(H["Successfully uploaded file!"]);
+
+            return RedirectToAction(nameof(UploadFileToMedia));
+        }
+        public async Task<string> CreateFileInCustomFolder()
+        {
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes("Gov file storage!")))
+            {
+                await _govFileStore.CreateFileFromStreamAsync(TestFileRelativePath, stream, true);
+            }
+
+            var fileInfo = await _govFileStore.GetFileInfoAsync(TestFileRelativePath);
+
+            return $"Successfully created file in the custom file storage! File size: {fileInfo.Length} bytes.";
+        }
+    }
+}
--- INZFS.MVC/Startup.cs
+++ INZFS.MVC/Startup.cs
@@ -4,6 +4,7 @@ using INZFS.MVC.Forms;
 
 using INZFS.MVC.Migrations;
 using INZFS.MVC.Models;
+using INZFS.MVC.Services;
 using INZFS.MVC.TagHelpers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
@@ -13,6 +14,9 @@ using OrchardCore.ContentManagement.Display.ContentDisplay;
 using OrchardCore.Data.Migration;
 using OrchardCore.DisplayManagement.TagHelpers;
 using OrchardCore.Modules;
+using Microsoft.Extensions.Options;
+using OrchardCore.Environment.Shell;
+using System.IO;
 
 
 namespace INZFS.MVC
@@ -25,7 +29,7 @@ namespace INZFS.MVC
             services.AddTagHelpers<AddClassTagHelper>();
             services.AddTagHelpers<ValidationMessageTagHelper>();
             services.AddTagHelpers<ValidationHighLighterTagHelper>();
-           
+
             services.AddContentPart<ProjectSummaryPart>()
               .UseDisplayDriver<ProjectSummaryDriver>();
             services.AddScoped<IDataMigration, ProjectSummaryMigration>();
@@ -39,6 +43,27 @@ namespace INZFS.MVC
             services.AddScoped<IDataMigration, OrgFundingMigration>();
 
             services.AddScoped<INavigation, Navigation>();
+
+            services.AddSingleton<IGovFileStore>(serviceProvider =>
+            {
+
+                var shellOptions = serviceProvider.GetRequiredService<IOptions<ShellOptions>>().Value;
+                var shellSettings = serviceProvider.GetRequiredService<ShellSettings>();
+
+                var tenantFolderPath = PathExtensions.Combine(
+
+                    shellOptions.ShellsApplicationDataPath,
+
+                    shellOptions.ShellsContainerName,
+
+                    shellSettings.Name);
+
+
+                var customFolderPath = PathExtensions.Combine(tenantFolderPath, "CustomFiles");
+
+
+                return new GovFileStore(customFolderPath);
+            });
         }
 
         public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
