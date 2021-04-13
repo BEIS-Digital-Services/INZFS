13fbbf6f22606b2d5279a3f754a16a73f74610d3
Lorenzo Lane <lorenzo.lane@beis.gov.uk>
2021-04-13 08:15:10 +0000

    Added download link

--- INZFS.MVC/Controllers/FundApplicationController.cs
+++ INZFS.MVC/Controllers/FundApplicationController.cs
@@ -258,6 +258,7 @@ namespace INZFS.MVC.Controllers
                     }
                     else
                     {
+                        ViewBag.Message = "Upload Successful!";
                         return await Edit(projectSummary.ContentItemId, projectSummary.ContentType);
                     }
                 }
