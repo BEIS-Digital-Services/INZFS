33652eb9d53b72a847512c5d909820af441b006a
Ben Lander <ben@benlander.me>
2021-05-20 08:01:37 +0000

    Breaking ReportController into its own file

---
+++ INZFS.MVC/Controllers/ReportController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INZFS.MVC.Controllers
+{
+    public class ReportController : ControllerBase
+    {
+        private readonly IReportService _reportService;
+        public ReportController(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+        [HttpGet]
+        public FileContentResult GeneratePdf()
+        {
+            var pdfFile = _reportService.GeneratePdfReport();
+            return File(pdfFile, "application/octet-stream", "Placeholder.pdf");
+        }
+    }
+}
