59c057632e3d5893604e91ac0219bcadfe3cbca6
Ben Lander <ben@benlander.me>
2021-06-03 08:46:45 +0000

    Fixing typos in variable name

--- INZFS.MVC/Controllers/AdminController.cs
+++ INZFS.MVC/Controllers/AdminController.cs
@@ -49,7 +49,7 @@ namespace INZFS.MVC.Controllers
 
         private async Task<Dictionary<string, ContentItem>> GetContentItemListFromBagPart(string companyName)
         {
-            var applicatinListResult = new Dictionary<string, ContentItem>();
+            var applicationListResult = new Dictionary<string, ContentItem>();
 
             var query = _session.Query<ContentItem, ContentItemIndex>();
             query = query.With<ContentItemIndex>(index => index.ContentType == ContentTypes.INZFSApplicationContainer);
@@ -68,14 +68,14 @@ namespace INZFS.MVC.Controllers
 
                     if (companyDetailsPart.CompanyName.ToLower().Contains(companyName.ToLower()))
                     {
-                        applicatinListResult.Add(companyDetailsPart.CompanyName, application);
+                        applicationListResult.Add(companyDetailsPart.CompanyName, application);
                     }
                 }
 
             }
 
 
-            return applicatinListResult;
+            return applicationListResult;
         }
 
         [HttpGet]
