474f464191d83a05f527aab3f0cd05ace16198c8
Glen Hani <m.hani@live.co.uk>
2021-06-16 10:10:21 +0100

    commit

---
+++ INZFS.MVC/Models/DynamicForm/BasePart.cs
@@ -0,0 +1,24 @@
+﻿using OrchardCore.ContentManagement;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INZFS.MVC.Models.DynamicForm
+{
+    public class BasePart : ContentPart
+    {
+
+        public string Name { get; set; }
+        public string Question { get; set; }
+        public string ErrorMessage { get; set; }
+        public string FieldType { get; set; }
+        public bool? Mandetory { get; set; }
+
+ 
+
+    }
+
+}
