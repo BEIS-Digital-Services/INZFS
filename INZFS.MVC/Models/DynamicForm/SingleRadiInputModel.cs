c4217a58274be22d77dd7bcebd05ac9650ca9c2e
Glen Hani <m.hani@live.co.uk>
2021-06-17 16:08:54 +0100

    Radi Complete

---
+++ INZFS.MVC/Models/DynamicForm/SingleRadiInputModel.cs
@@ -0,0 +1,17 @@
+﻿using OrchardCore.ContentManagement;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INZFS.MVC.Models.DynamicForm
+{
+    public class SingleRadiInputModel : BaseModel
+    {
+        public bool DataInput { get; set; } = false;
+
+    }
+}
