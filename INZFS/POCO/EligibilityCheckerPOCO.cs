54ce9e8e4c26db50c3a67329329e4c0368c06f7f
Lorenzo Lane <lorenzo.lane@beis.gov.uk>
2021-06-17 21:39:26 +0000

    Added redirects

---
+++ INZFS/POCO/EligibilityCheckerPOCO.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INZFS.Web.POCO
+{
+    public class EligibilityCheckerPOCO
+    {
+        
+        public bool Question1 { get; set; } 
+       
+        public bool Question2 { get; set; } 
+       
+        public bool Question3 { get; set; } 
+        
+        public bool Question4 { get; set; } 
+       
+        public bool Question5 { get; set; } 
+   
+        public bool Question6 { get; set; } 
+        [BindProperty]
+        public string RedirectUrl { get; set; }
+
+        public EligibilityCheckerPOCO()
+        {
+            RedirectUrl = "/";
+        }
+    }
+   
+}
