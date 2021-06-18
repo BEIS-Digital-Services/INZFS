b759ee3a79f7c36f44e260c88075387b1e363982
imran.ali <imran.ali@RP-DEV-28>
2021-06-18 08:02:42 +0000

    Added AutomatedUITest Project

---
+++ NetZero.Automated.UI.Tests/NetZero.Automated.UI.Tests/Steps/LoginSteps.cs
@@ -0,0 +1,76 @@
+﻿using NetZero.Automated.UI.Tests.Pages;
+using NetZero.Automated.UI.Tests.Utils;
+using TechTalk.SpecFlow;
+
+namespace NetZero.Automated.UI.Tests.Steps
+{
+    [Binding]
+    public sealed class LoginSteps
+    {
+        private readonly ScenarioContext scenarioContext;
+
+        public LoginSteps(ScenarioContext scenarioContext)
+        {
+            this.scenarioContext = scenarioContext;
+            this.scenarioContext.Set(new LoginPage(scenarioContext));
+        }
+
+        [Given(@"I am on the login page")]
+        public void GivenTheUserIsOnTheLoginPage()
+        {
+            scenarioContext.Get<LoginPage>().Open();
+        }
+
+        [When(@"I submit valid login details")]
+        public void WhenTheUserSubmitsValidLoginDetails()
+        {
+            scenarioContext.Get<LoginPage>().LoginTestUser();
+        }
+
+        [When(@"I submit invalid login details")]
+        public void WhenTheUserSubmitsInvalidLoginDetails()
+        {
+            scenarioContext.Get<LoginPage>().LoginWithInvalidDetails();
+        }
+
+        [When(@"I submit with the below combination of field values then I should see the FieldValidation and ValidationSummary accordingly")]
+        public void WhenISubmitWithTheBelowCombinationOfFieldValuesThenIShouldSeeTheFieldValidationAndValidationSummaryAccordingly(Table table)
+        {
+            scenarioContext.Get<Logger>().Log("Verifying field and summary level validations...");
+            foreach (var row in table.Rows)
+            {
+                scenarioContext.Get<Logger>().LogMessage($"Entering Username '{row["UsernameValue"]}' and password '{row["PasswordValue"]}'...");
+                scenarioContext.Get<LoginPage>().EnterLoginValuesAndSubmit(row["UsernameValue"], row["PasswordValue"]);
+                scenarioContext.Get<LoginPage>().VerifyIncompleteLoginEntryValidation(row["UsernameValue"], row["PasswordValue"], row["FieldValidation"], row["ValidationSummary"]);
+            }
+        }
+
+        [Then(@"I should see relevant error message")]
+        public void ThenIShouldSeeRelevantErrorMessage()
+        {
+            scenarioContext.Get<LoginPage>().VerifyInvalidLoginValidation();
+        }
+
+        [Then(@"I should remain on the Login page")]
+        public void ThenIShouldRemainOnTheLoginPage()
+        {
+            scenarioContext.Get<LoginPage>().VerifyLoginPageIsOpen();
+        }
+
+        [Then(@"I am able to access application page")]
+        public void ThenIAmAbleToAccessApplicationPage()
+        {
+            scenarioContext.Get<LoginPage>().VerifyApplicationIsAccessible();
+        }
+
+        [Given(@"I have logged in with the new user details")]
+        public void GivenIHaveLoggedIn()
+        {
+            scenarioContext.Get<LoginPage>().LoginNewUser();
+        }
+
+
+    }
+
+
+}
---
+++ NetZero.Automated.UI.Tests/NetZero.Automated.UI.Tests/Utils/Hooks.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using BEIS.GDS.UI.Tests.Support.Driver;
+using NetZero.Automated.UI.Tests.Helpers;
+using NetZero.Automated.UI.Tests.Pages;
+using NetZero.Automated.UI.Tests.Pages.CMS;
+using NetZero.Automated.UI.Tests.TestSetUp;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace NetZero.Automated.UI.Tests.Utils
+{
+
+    [Binding]
+    public sealed class Hooks
+    {
+        private ScenarioContext context;
+        
+        private static AventStack.ExtentReports.ExtentReports extent;
+        private static ExtentTest feature;
+        private static ExtentTest scenario;
+        public Hooks(ScenarioContext context)
+        {
+            this.context = context;
+        }
+
+        [BeforeTestRun]
+        public static void ReportInitialize()
+        {
+            extent = new AventStack.ExtentReports.ExtentReports();
+            extent.AttachReporter(ReportHelpers.InitializeAndGetReporter());
+            ReportHelpers.TimeStamp = DateTime.Now.ToString("ddMMyy");
+        }
+
+        [BeforeFeature]
+        public static void BeforeFeature(FeatureContext featureContext)
+        {
+            feature = extent.CreateTest<Feature>(featureContext.FeatureInfo.Title);
+        }
+
+        [BeforeScenario("UserCleanUp")]
+        public void UserCleanUp()
+        {
+            context.Set(new LoginPage(context));
+            context.Set(new UsersPage(context));
+            context.Get<Logger>().Log("Test user clean up starting...");
+            context.Get<UsersPage>().DeleteTestUsers();
+            context.Get<Logger>().Log("Test user clean up done.");
+        }
+
+        [BeforeScenario(Order = 0)]
+        public void BeforeScenario()
+        {
+            scenario = feature.CreateNode<Scenario>(context.ScenarioInfo.Title);
+            context.Set(new DriverSetUp().SetUpDriver(TestSettings.Options));
+            context.Get<IWebDriver>().Navigate().GoToUrl(ConfigurationSetUp.BaseUrl);
+            context.Set(new Logger());
+            context.Set(new ReportHelpers(context));
+            context.Get<Logger>().Log($"Scenario: {context.ScenarioInfo.Title}");
+            context.Get<Logger>().LogMessage($"---------------------------------------------------------");
+        }
+
+
+        [AfterScenario]
+        public void AfterScenario(FeatureContext featureContext)
+        {
+            extent.Flush();
+            context.Get<IWebDriver>().Quit();
+            context.Get<ReportHelpers>().AttachTestArtifact();
+            TestContext.AddTestAttachment(context.Get<Logger>().FilePath);
+            context.Clear();
+        }
+
+        [BeforeStep]
+        public void BeforeStep()
+        {
+            context.Get<Logger>().LogToFile($"__________________________________________________________________________");
+            context.Get<Logger>().LogToFile($"\r\n{DateTime.Now.ToString("dd/MM/yy HH:mm:ss")} | LOG STEP NAME: {context.StepContext.StepInfo.StepDefinitionType} {context.StepContext.StepInfo.Text}");
+        }
+
+        [AfterStep]
+        public void AfterStep()
+        {
+            var text = context.TestError == null ? $" --> done: {context.StepContext.StepInfo.Text}" : " --> ERROR :";
+            context.Get<Logger>().LogToFile(text);
+            context.Get<ReportHelpers>().InsertStepsInReport(feature, scenario);
+
+        }
+    }
+}
