ce9e9f09b4f0562607921558017f4d8338580b9a
GardinerD <david.gardiner@beis.gov.uk>
2021-06-29 13:37:41 +0000

    adding selinium test

---
+++ INZFS.UnitTests/SelenuimTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace INZFS.UnitTests
+{
+    class SeleniumTest
+    {
+        [Test]
+        [Category("UITests")]
+        public void VisitMicrosoft_CheckWindowsMenu()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://inzfs-lab.london.cloudapps.digital/");
+            Thread.Sleep(10000);
+            string Windows_text = driver.FindElement(By.Id("shellmenu_1")).Text;
+            Assert.AreEqual("StartNow", Windows_text);
+            driver.Quit();
+        }
+    }
+}
