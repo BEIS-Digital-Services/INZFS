5057981001b85584dc221728f71aebf19e555118
Lorenzo Lane <lorenzo.lane@beis.gov.uk>
2021-08-16 11:30:26 +0000

    Added MaxValidation to TextBox field

--- INZFS.MVC/Models/DynamicForm/TextInputModel.cs
+++ INZFS.MVC/Models/DynamicForm/TextInputModel.cs
@@ -18,6 +18,24 @@ namespace INZFS.MVC.Models.DynamicForm
                 {
                     yield return new ValidationResult(ErrorMessage, new[] { nameof(DataInput) });
                 }
+                else
+                {
+                    if (CurrentPage.MaxLengthValidationType == MaxLengthValidationType.Character)
+                    {
+                        if (DataInput.Length > CurrentPage.MaxLength)
+                        {
+                            yield return new ValidationResult($"{CurrentPage.FriendlyFieldName} must be {CurrentPage.MaxLength} characters or fewer", new[] { nameof(DataInput) });
+                        }
+                    }
+                    else
+                    {
+                        var numberOfWords = DataInput.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+                        if (numberOfWords > CurrentPage.MaxLength)
+                        {
+                            yield return new ValidationResult($"{CurrentPage.FriendlyFieldName} must be {CurrentPage.MaxLength} words or fewer", new[] { nameof(DataInput) });
+                        }
+                    }
+                }
             }
         }
     }
