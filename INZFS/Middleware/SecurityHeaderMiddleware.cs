7229643419922947db7a846533d3c3aec0a800e7
Lorenzo Lane <lorenzo.lane@beis.gov.uk>
2021-08-24 14:48:06 +0000

    Implemented suggested click-jacking fix (Ticket 3 in Pen Test fixes document)

---
+++ INZFS/Middleware/SecurityHeaderMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INZFS.Web.Middleware
+{
+    public class SecurityHeaderMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public SecurityHeaderMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            context.Response.Headers.Add("referrer-policy", new StringValues("strict-origin-when-cross-origin"));
+           
+            context.Response.Headers.Add("x-content-type-options", new StringValues("nosniff"));
+   
+            context.Response.Headers.Add("x-frame-options", new StringValues("DENY"));
+
+            context.Response.Headers.Add("X-Permitted-Cross-Domain-Policies", new StringValues("none"));
+
+            context.Response.Headers.Add("x-xss-protection", new StringValues("1; mode=block"));
+
+            context.Response.Headers.Add("frame-ancestors", new StringValues("none"));
+
+            return _next(context);
+        }
+    }
+
+}
--- INZFS/Startup.cs
+++ INZFS/Startup.cs
@@ -63,7 +63,7 @@ namespace INZFS
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseMiddleware<SecurityHeaderMiddleware>();
             app.UseSession();
             app.UseVcapSession();
             app.UseStaticFiles();
