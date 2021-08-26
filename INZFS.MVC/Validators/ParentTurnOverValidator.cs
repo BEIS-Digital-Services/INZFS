3b255fb4d4ce2b91a505b45ff8f347c14801da97
Glen Hani <m.hani@live.co.uk>
2021-08-26 11:43:44 +0100

    Removed Custom Validation from currency model to ParentTurnOverValidator

---
+++ INZFS.MVC/Validators/ParentTurnOverValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INZFS.MVC.Validators
+{
+    public class ParentTurnOverValidator : ICustomValidator
+    {
+        public IEnumerable<ValidationResult> Validate(string dataInput, string friendlyFriendlyFieldName)
+        {
+
+            var numberOnly = dataInput.Replace(",", "");
+            double currencyValue = 0.0;
+            if (double.TryParse(numberOnly, out currencyValue))
+            {
+                if (currencyValue > 1500000)
+                {
+                    yield return new ValidationResult($"Your parent company's {friendlyFriendlyFieldName} cannot be greater than £1,500,000", new[] { nameof(dataInput) });
+                }
+            }
+            else
+            {
+                yield return new ValidationResult($"{friendlyFriendlyFieldName} must only include numbers, commas and full-stops", new[] { nameof(dataInput) });
+            }
+
+        }
+    }
+}
