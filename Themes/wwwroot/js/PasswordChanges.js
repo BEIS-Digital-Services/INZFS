950c587ea329e609f2b9088b6bb71344cbb1b35b
Lorenzo Lane <lorenzo.lane@beis.gov.uk>
2021-08-27 08:15:39 +0100

    Added javascript files separately and removed nonces

---
+++ Themes/wwwroot/js/PasswordChanges.js
@@ -0,0 +1,21 @@
+﻿function createPassword() {
+    var x = document.getElementById("Password");
+    if (x.type === "password") {
+        x.type = "text";
+        document.getElementById("buttonText").innerHTML = "Hide";
+    } else {
+        x.type = "password";
+        document.getElementById("buttonText").innerHTML = "Show";
+    }
+}
+
+function confirmPassword() {
+    var x = document.getElementById("ConfirmPassword");
+    if (x.type === "password") {
+        x.type = "text";
+        document.getElementById("buttonText1").innerHTML = "Hide";
+    } else {
+        x.type = "password";
+        document.getElementById("buttonText1").innerHTML = "Show";
+    }
+}
