a96d4d662806b4ff38c5b2dd5e58651fcf320fc9
Lorenzo Lane <lorenzo.lane@beis.gov.uk>
2021-09-01 08:16:55 +0100

    QR code fix tests

--- INZFS/Middleware/SecurityHeaderMiddleware.cs
+++ INZFS/Middleware/SecurityHeaderMiddleware.cs
@@ -35,7 +35,7 @@ namespace INZFS.Web.Middleware
                 "font-src 'self' https://www.gov.uk/assets/static/fonts/;" +
                 "frame-ancestors 'none';" +
                 "frame-src 'none';" +
-                "img-src 'self' http://www.w3.org/2000/svg;" +
+                "img-src 'self' data: http://www.w3.org/2000/svg https://www.zcloud.net otpauth://totp ;" +
                 "media-src 'self';" +
                 "object-src 'self';" +
                 "script-src 'self' https://www.googletagmanager.com/gtag/ https://ajax.aspnetcdn.com/ajax/jquery/jquery-3.6.0.min.js https://code.jquery.com/jquery-3.6.0.js https://design-system.service.gov.uk/javascripts/govuk-frontend-d7b7e40c8ac2bc81d184bb2e92d680b9.js sha256-E041E97A2AEEDAB8A6BB8C125A11C5F399004569D3681967DCEAE852F5D26EB1 sha256-5BF7094BFF6D37EFF5AB6973DAADE41133478BC82216EA8CDB033D850C3EC4E1 ;"
