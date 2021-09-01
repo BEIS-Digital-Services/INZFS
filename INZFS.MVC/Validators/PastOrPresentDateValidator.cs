4eb3e97fa81d8c94309cffedcf703b8b5f6bade2
Glen Hani <m.hani@live.co.uk>
2021-09-01 10:25:31 +0100

    Date Logic Validation: Past Dates, Date Not Fully Completed, Invalid formate logic AND Added Past Date Validation to Balance Sheet Date

---
+++ INZFS.MVC/Validators/PastOrPresentDateValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INZFS.MVC.Validators
+{
+    public class PastOrPresentDateValidator : ICustomValidator
+    {
+        public IEnumerable<ValidationResult> Validate(string dataInput, string friendlyFriendlyFieldName)
+        {
+            DateTime userDateInput;
+            DateTime currentDate = DateTime.UtcNow.Date;
+            if (DateTime.TryParseExact(dataInput, "d/M/yyyy", CultureInfo.CurrentCulture, DateTimeStyles.None, out userDateInput))
+            {
+                if (userDateInput > currentDate)
+                {
+                    yield return new ValidationResult($"{friendlyFriendlyFieldName} must be in the past", new[] { "DateUtc" });
+                }
+            }
+        }
+    }
+}
