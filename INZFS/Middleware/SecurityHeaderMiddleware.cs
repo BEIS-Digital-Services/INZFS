43a0639f6bdb84bb30f0521c90b86d4f078fbf37
Lorenzo Lane <lorenzo.lane@beis.gov.uk>
2021-09-01 13:10:43 +0100

    Fixed QR code issues

--- INZFS/Middleware/SecurityHeaderMiddleware.cs
+++ INZFS/Middleware/SecurityHeaderMiddleware.cs
@@ -38,7 +38,7 @@ namespace INZFS.Web.Middleware
                 "img-src 'self' data: http://www.w3.org/2000/svg https://www.zcloud.net otpauth://totp ;" +
                 "media-src 'self';" +
                 "object-src 'self';" +
-                "script-src 'self' https://www.googletagmanager.com/gtag/ https://ajax.aspnetcdn.com/ajax/jquery/jquery-3.6.0.min.js https://code.jquery.com/jquery-3.6.0.js https://design-system.service.gov.uk/javascripts/govuk-frontend-d7b7e40c8ac2bc81d184bb2e92d680b9.js sha256-E041E97A2AEEDAB8A6BB8C125A11C5F399004569D3681967DCEAE852F5D26EB1 sha256-5BF7094BFF6D37EFF5AB6973DAADE41133478BC82216EA8CDB033D850C3EC4E1 ;"
+                "script-src 'self' https://www.googletagmanager.com/gtag/ https://ajax.aspnetcdn.com/ajax/jquery/jquery-3.6.0.min.js https://code.jquery.com/jquery-3.6.0.js https://design-system.service.gov.uk/javascripts/govuk-frontend-d7b7e40c8ac2bc81d184bb2e92d680b9.js 'sha256-E041E97A2AEEDAB8A6BB8C125A11C5F399004569D3681967DCEAE852F5D26EB1' 'sha256-5BF7094BFF6D37EFF5AB6973DAADE41133478BC82216EA8CDB033D850C3EC4E1' 'nonce-BB60C4898C5AC94547F875BA438739DC6B2299EC3152ADAE0E0C3370F31BC35A' ;"
             )); ;
 
             return _next(context);
