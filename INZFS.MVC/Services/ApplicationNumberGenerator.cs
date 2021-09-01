1fcf1b2b79d1cb531acac3ebb0aecb9e1ce86151
Sunil DSouza <sunil.dsouza@beis.gov.uk>
2021-09-01 19:59:59 +0100

    Application number generation

---
+++ INZFS.MVC/Services/ApplicationNumberGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INZFS.MVC.Services
+{
+    public interface IApplicationNumberGenerator
+    {
+        public string Generate(int size);
+    }
+    public class ApplicationNumberGenerator : IApplicationNumberGenerator
+    {
+        const string PREFIX = "EEF9";
+        public string Generate(int size)
+        {
+            char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+            byte[] data = new byte[size];
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                crypto.GetBytes(data);
+            }
+            StringBuilder result = new StringBuilder(size);
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length)]);
+            }
+            return PREFIX + "" + result.ToString().ToUpper();
+        }
+    }
+}
