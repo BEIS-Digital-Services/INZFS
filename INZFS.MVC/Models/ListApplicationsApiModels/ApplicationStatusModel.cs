7276739744959b1a70b71334b88984b8c4ec4306
Lorenzo Lane <lorenzo.lane@beis.gov.uk>
2021-09-06 07:54:31 +0100

    Added status model

---
+++ INZFS.MVC/Models/ListApplicationsApiModels/ApplicationStatusModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INZFS.MVC.Models.ListApplicationsApiModels
+{
+    public class ApplicationStatusModel
+    {
+        public int ID { get; set; }
+        public string ApplicationName { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
