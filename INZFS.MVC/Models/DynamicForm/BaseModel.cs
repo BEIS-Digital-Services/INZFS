c1220e0e36e2732a38fb342bbfa9a2a07d9a2960
Sunil DSouza <sunil.dsouza@beis.gov.uk>
2021-09-11 10:23:57 +0100

    Removed showSaveProgessButton option

--- INZFS.MVC/Models/DynamicForm/BaseModel.cs
+++ INZFS.MVC/Models/DynamicForm/BaseModel.cs
@@ -38,7 +38,6 @@ namespace INZFS.MVC.Models.DynamicForm
         public int? MaxLength { get; set; }
         public string NextPageName { get; set; }
         public string ReturnPageName { get; set; }
-        public bool ShowSaveProgessButton { get; set; }
         public string ReturnToSummaryPageLinkText { get; set; }
         public string ContinueButtonText { get; set; }
         public string SectionUrl { get; set; }
