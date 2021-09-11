fa559cfc66d8f140487b9e49db8ae8414876dae5
Glen Hani <m.hani@live.co.uk>
2021-09-11 09:55:06 +0100

    Javascript for section 7.9

---
+++ INZFS/wwwroot/js/ToggleOtherOption.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+
+    $("#checkboxSelect-other").click(function () {
+        if ($(this).is(":checked")) {
+            $(".hide-input").show();
+        } else {
+            $(".hide-input").hide();
+        }
+    });
+
+});
