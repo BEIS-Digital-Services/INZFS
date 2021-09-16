2613fc59eeae2a5880af3873fdc4399d2b0632c6
Sunil DSouza <sunil.dsouza@beis.gov.uk>
2021-09-16 19:32:33 +0100

    Application submitted page

---
+++ INZFS.MVC/Extensions/ApplicationStatusExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INZFS.MVC.Extensions
+{
+    public static class ApplicationStatusExtension
+    {
+        public static string ToStatusString(this ApplicationStatus status)
+        {
+            switch (status)
+            {
+                case ApplicationStatus.InProgress:
+                    return "In Progress";
+                case ApplicationStatus.Submitted:
+                    return "Submitted";
+                case ApplicationStatus.Assessment:
+                    return "Submitted";
+                case ApplicationStatus.IndependentAssessment:
+                    return "Independen tAssessment";
+                case ApplicationStatus.Successful:
+                    return "Successful";
+                case ApplicationStatus.Unsuccessful:
+                    return "Unsuccessful";
+                case ApplicationStatus.Withdrawn:
+                    return "Withdrawn";
+                default:
+                    throw new Exception("Invalid status");
+            }
+        }
+    }
+
+    
+}
