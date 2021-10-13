bf6303940034f80be727fe0bee9cba82a13946db
fathirAtBeis <fathir.miah@beis.gov.uk>
2021-10-13 10:23:08 +0100

    Breadcrumb is incorrect on your account

---
+++ Themes/Models/BreadcrumbsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INZFS.Theme.Models
+{
+    public class BreadcrumbsModel
+    {
+        public string Url { get; set; }
+        public bool IsDisplay { get; set; }
+    }
+}
