28b4d1d83616b793c80f84537402e8003d2ea208
fathirAtBeis <fathir.miah@beis.gov.uk>
2021-10-21 16:34:31 +0100

    ThisCodeMustNotGoLiveAntiforgery

---
+++ Themes/ThisCodeMustNotGoLiveAntiforgery.cs
@@ -0,0 +1,44 @@
+﻿
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INZFS.Theme
+{
+    /// <summary>
+    /// this code must not go live as it will be an issue with CSRF for anonymous forms. this fix should take as a spike only
+    /// </summary>
+    public class ThisCodeMustNotGoLiveAntiforgery : IAntiforgery
+    {
+        public AntiforgeryTokenSet GetAndStoreTokens(HttpContext httpContext)
+        {
+            //HACK:Dummy tocken for form and cookies
+            return new AntiforgeryTokenSet("token", "token", "tovalidate", "header");
+        }
+
+        public AntiforgeryTokenSet GetTokens(HttpContext httpContext)
+        {
+            //HACK:Dummy tocken for form and cookies
+            return new AntiforgeryTokenSet("token", "token", "tovalidate", "header");
+        }
+
+        public async Task<bool> IsRequestValidAsync(HttpContext httpContext)
+        {
+            return true;
+        }
+
+        public void SetCookieTokenAndHeader(HttpContext httpContext)
+        {
+
+        }
+
+        public async Task ValidateRequestAsync(HttpContext httpContext)
+        {
+
+        }
+    }
+}
