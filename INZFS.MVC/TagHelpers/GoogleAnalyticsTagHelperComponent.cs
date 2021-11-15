911b9241fdb9d50f808da23414d95da31d31af4a
Lorenzo Lane <lorenzo.lane@beis.gov.uk>
2021-11-15 16:08:49 +0000

    Added GA tag helpers

---
+++ INZFS.MVC/TagHelpers/GoogleAnalyticsTagHelperComponent.cs
@@ -0,0 +1,42 @@
+﻿using INZFS.MVC.Settings;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INZFS.MVC.TagHelpers
+{
+    public class GoogleAnalyticsTagHelperComponent : TagHelperComponent
+    {
+        private readonly GoogleAnalyticsOptions _googleAnalyticsOptions;
+
+        public GoogleAnalyticsTagHelperComponent(IOptions<GoogleAnalyticsOptions> googleAnalyticsOptions)
+        {
+            _googleAnalyticsOptions = googleAnalyticsOptions.Value;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if(string.Equals(output.TagName, "head", StringComparison.OrdinalIgnoreCase))
+            // Inject the code only in the head element
+            if (string.Equals(output.TagName, "head", StringComparison.OrdinalIgnoreCase))
+            {
+                // Get the tracking code from the configuration
+                var trackingCode = _googleAnalyticsOptions.TrackingCode;
+                if (!string.IsNullOrEmpty(trackingCode))
+                {
+                    // PostContent correspond to the text just before closing tag
+                    output.PostContent
+                        .AppendHtml("<script async src='https://www.googletagmanager.com/gtag/js?id=")
+                        .AppendHtml(trackingCode)
+                        .AppendHtml("'></script><script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date);gtag('config','")
+                        .AppendHtml(trackingCode)
+                        .AppendHtml("',{displayFeaturesTask:'null'});</script>");
+                }
+            }
+        }
+    }
+}
