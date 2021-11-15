c4e7d3a3a3a368b2995853ec308efb6199220c3e
FathirAtBeis <fathir.miah@beis.gov.uk>
2021-11-15 14:38:47 +0000

    IN-1845 #22 (Dev Ready) [A11y] 1.1.1 Non-Text Content - Alternative Text Not Present For Non-Decorative Imagery

--- Themes/wwwroot/lib/qrcode-activate.js
+++ Themes/wwwroot/lib/qrcode-activate.js
@@ -1,8 +1,16 @@
 ﻿
-var element = document.getElementById("qrCode");
-new QRCode(element,
-    {
-        text: element.getAttribute("code"),
-        width: 150,
-        height: 150
-    });
+
+
+$(document).ready(function () {
+
+    var element = document.getElementById("qrCode");
+    new QRCode(element,
+        {
+            text: element.getAttribute("code"),
+            width: 150,
+            height: 150
+        });
+
+    $("img", $("#qrCode")).attr("alt", "QR Code");
+
+});
