f18b95001b4c715e5481127275b8c73bec5d4a44
Ben Lander <ben@benlander.me>
2021-11-16 09:59:06 +0000

    Refactor

--- INZFS.MVC/Controllers/ReportController.cs
+++ INZFS.MVC/Controllers/ReportController.cs
@@ -21,10 +21,13 @@ namespace INZFS.MVC.Controllers
     {
         private readonly IReportService _reportService;
         private IWebHostEnvironment _env;
+        private string _logoFilepath;
         public ReportController(IReportService reportService, IWebHostEnvironment env)
         {
             _reportService = reportService;
             _env = env;
+            _logoFilepath = Path.Combine(_env.WebRootPath, "assets", "images", "beis_logo.png");
+
         }
 
         public async Task<FileContentResult> DownloadApplication()
@@ -44,8 +47,13 @@ namespace INZFS.MVC.Controllers
 
         private async Task<ReportContent> GetPdfContent()
         {
-            string logoFilepath = Path.Combine(_env.WebRootPath, "assets", "images", "beis_logo.png");
-            var reportContent = await _reportService.GeneratePdfReport(GetUserId(), logoFilepath);
+            var reportContent = await _reportService.GeneratePdfReport(GetUserId(), _logoFilepath);
+            return reportContent;
+        }
+
+        private async Task<ReportContent> GetOdtContent()
+        {
+            var reportContent = await _reportService.GenerateOdtReport(GetUserId(), _logoFilepath);
             return reportContent;
         }
 
@@ -55,14 +63,11 @@ namespace INZFS.MVC.Controllers
             var reportContent = await GetPdfContent();
             return File(reportContent.FileContents, "application/pdf", $"Application Form {reportContent.ApplicationNumber}.pdf");
         }
-        public async Task<FileContentResult> GenerateOdt()
-        {
-            string logoFilepath = Path.Combine(_env.WebRootPath, "assets", "images", "beis_logo.png");
-            var reportContent = await _reportService.GenerateOdtReport(GetUserId(), logoFilepath);
-            string type = "application/vnd.oasis.opendocument.text";
-            string name = $"Application Form {reportContent.ApplicationNumber}.odt";
 
-            return File(reportContent.FileContents, type, name);
+        public async Task<FileContentResult> DownloadOdt()
+        {
+            var reportContent = await GetOdtContent();
+            return File(reportContent.FileContents, "application/vnd.oasis.opendocument.text", $"Application Form {reportContent.ApplicationNumber}.odt");
         }
 
         private string GetUserId()
