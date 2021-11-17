70de2942986dc3e5a3083a21f56aaa238e5fcb34
Ben Lander <ben@benlander.me>
2021-11-17 16:18:38 +0000

    Initial azure implementation

--- INZFS.MVC/Controllers/ReportController.cs
+++ INZFS.MVC/Controllers/ReportController.cs
@@ -16,6 +16,8 @@ using INZFS.MVC.Services;
 using System.Text.Json;
 using OrchardCore.Media;
 using OrchardCore.FileStorage;
+using Microsoft.Extensions.Configuration;
+using Azure.Storage.Blobs;
 
 namespace INZFS.MVC.Controllers
 {
@@ -27,14 +29,16 @@ namespace INZFS.MVC.Controllers
         private string _logoFilepath;
         private readonly IContentRepository _contentRepository;
         private readonly IMediaFileStore _mediaFileStore;
+        private readonly IConfiguration _configuration;
 
-        public ReportController(IReportService reportService, IWebHostEnvironment env, IContentRepository contentRepository, IMediaFileStore mediaFileStore)
+        public ReportController(IReportService reportService, IWebHostEnvironment env, IContentRepository contentRepository, IMediaFileStore mediaFileStore, IConfiguration configuration)
         {
             _reportService = reportService;
             _env = env;
             _logoFilepath = Path.Combine(_env.WebRootPath, "assets", "images", "beis_logo.png");
             _contentRepository = contentRepository;
             _mediaFileStore = mediaFileStore;
+            _configuration = configuration;
         }
 
         public async Task<FileContentResult> DownloadApplication(string filetype)
@@ -68,12 +72,40 @@ namespace INZFS.MVC.Controllers
 
                     foreach(var file in uploadedFiles)
                     {
-                        //string path = (env == "Development") ? _mediaFileStore.NormalizePath("/App_Data/Sites/Default" + file.FileLocation) : _mediaFileStore.NormalizePath(file.FileLocation);
-                        var zipArchiveEntry = archive.CreateEntryFromFile(file.FileLocation, Path.Combine("Uploaded Documents", file.Name));
+                        BinaryData binaryData = await GetFileFromBlobStorage(file);
+                        var stream = binaryData.ToStream();
+
+                        byte[] bytes;
+                        using (var streamReader = new MemoryStream())
+                        {
+                            stream.CopyTo(streamReader);
+                            bytes = streamReader.ToArray();
+                        }
+
+                        var fileToArchive = archive.CreateEntry($"{file.Name}", CompressionLevel.Fastest);
+                        using (var zipStream = fileToArchive.Open()) zipStream.Write(bytes, 0, bytes.Length);
                     }
                 }
 
                 return File(ms.ToArray(), "application/zip", $"{reportContent.ApplicationNumber}.zip");
+            }
+        }
+
+        private async Task<BinaryData> GetFileFromBlobStorage(UploadedFile file)
+        {
+            try
+            {
+                string connectionString = _configuration["AzureBlobStorage"];
+                string containerName = "appdatasandbox";
+                string blobName = file.FileLocation;
+
+                var blob = new BlobClient(connectionString, containerName, blobName).DownloadContent().Value;
+                return blob.Content;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error getting file {file.Name} from Blob Storage: " + e.Message);
+                return null;
             }
         }
 
