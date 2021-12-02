902ed8dcc4aae61211af869bfe07882d928e30ef
Ben Lander <ben@benlander.me>
2021-12-02 16:10:38 +0000

    Removing hardcoded azure container lookup

--- INZFS.MVC/Controllers/ReportController.cs
+++ INZFS.MVC/Controllers/ReportController.cs
@@ -114,8 +114,7 @@ namespace INZFS.MVC.Controllers
             try
             {
                 string connectionString = _configuration["AzureBlobStorage"];
-                //TODO: Remove this hardcoded container name to env var
-                string containerName = "appdatasandbox";
+                string containerName = Environment.GetEnvironmentVariable("OrchardCore__OrchardCore_Shells_Azure__ContainerName");
                 string blobName = file.FileLocation.Replace("/media", "EEF");
 
                 var blob = new BlobClient(connectionString, containerName, blobName).DownloadContent().Value;
