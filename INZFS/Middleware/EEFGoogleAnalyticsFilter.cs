9f870857751527300b206d0ac82665b0e72c4842
Richard Anderson <rsanderson99@hotmail.com>
2021-12-03 15:31:02 +0000

    - Added check for Campaigns cookie tracking to stop GA code if users has chosen so

--- INZFS/Middleware/EEFGoogleAnalyticsFilter.cs
+++ INZFS/Middleware/EEFGoogleAnalyticsFilter.cs
@@ -13,6 +13,7 @@ using OrchardCore.Settings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace INZFS.Web.Middleware
@@ -43,13 +44,24 @@ namespace INZFS.Web.Middleware
             {
                 if (googleAnalyticsFilter._scriptsCache == null)
                 {
-                    string sNonce = _cspNonce.Nonces["PolicyGroup1"].Nonce;
+                    string sNonce = _cspNonce.Nonces["PolicyGroup1"].Nonce; // Quick FIX - Hard-coded for PolicyGroup1 - Nonce code needs to be re-factored
 
                     GoogleAnalyticsSettings analyticsSettings = (await googleAnalyticsFilter._siteService.GetSiteSettingsAsync()).As<GoogleAnalyticsSettings>();
                     if (!string.IsNullOrWhiteSpace(analyticsSettings?.TrackingID))
                         googleAnalyticsFilter._scriptsCache = new HtmlString("<!-- Global site tag (gtag.js) - Google Analytics -->\n<script async src=\"https://www.googletagmanager.com/gtag/js?id=" + analyticsSettings.TrackingID + "\"></script>\n<script nonce=\"" + sNonce + "\">window.dataLayer = window.dataLayer || [];function gtag() { dataLayer.push(arguments); }gtag('js', new Date());gtag('config', '" + analyticsSettings.TrackingID + "')</script>\n<!-- End Global site tag (gtag.js) - Google Analytics -->");
                 }
-                if (googleAnalyticsFilter._scriptsCache != null)
+                var bDoNotTrack = false;
+                var sCookiePolicy = context.HttpContext.Request.Cookies["cookies_policy"];
+                if (sCookiePolicy != null)
+				{
+                    try
+                    {
+                        JsonDocument doc = JsonDocument.Parse(sCookiePolicy);
+                        if (doc.RootElement.TryGetProperty("campaigns", out JsonElement elem) && elem.ToString() == "False") bDoNotTrack = true;
+                    }
+                    catch { }
+                }
+                if (googleAnalyticsFilter._scriptsCache != null && bDoNotTrack != true)
                     googleAnalyticsFilter._resourceManager.RegisterFootScript((IHtmlContent)googleAnalyticsFilter._scriptsCache);
             }
             ResultExecutedContext resultExecutedContext = await next();
