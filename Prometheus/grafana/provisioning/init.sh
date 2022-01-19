4e35a1d9cf32da84e9cf103aa2be4b21c94aeb90
DavidGardiner1 <77670239+DavidGardiner1@users.noreply.github.com>
2022-01-19 11:00:09 +0000

    Update init.sh

--- Prometheus/grafana/provisioning/init.sh
+++ Prometheus/grafana/provisioning/init.sh
@@ -9,14 +9,8 @@ sleep 10 &&
 curl \
  -XPOST \
  -H "Content-Type: application/json" \
- -d '{ "name":"Admin", "email":"admin@org.com", "isAdmin": true, "isDisabled": false, "login":"admin",  "password":"$(admin_password)" }' \
- http://admin:admin@localhost:3000/api/admin/users 
-
-curl \
- -XPOST \
- -H "Content-Type: application/json" \
  -d '{ "name":"viewer", "email":"viewer@org.com", "login":"viewer",  "password":"$(viewer_password)" }' \
- http://admin:$(admin_password)@localhost:3000/api/admin/users 
+ http://$(grafana_admin):$(grafana_password)@localhost:3000/api/admin/users 
 
 curl \
  -X PUT \
