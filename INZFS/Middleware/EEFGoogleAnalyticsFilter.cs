bc6af186e84dd146bb8137a4d9060ac06bf2e326
Lorenzo Lane <lorenzo.lane@beis.gov.uk>
2022-02-16 11:14:13 +0000

    INZFS.Web project cleanup

--- INZFS/Middleware/EEFGoogleAnalyticsFilter.cs
+++ INZFS/Middleware/EEFGoogleAnalyticsFilter.cs
@@ -4,15 +4,11 @@ using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Razor.TagHelpers;
 using OrchardCore.Admin;
 using OrchardCore.Entities;
 using OrchardCore.Google.Analytics.Settings;
 using OrchardCore.ResourceManagement;
 using OrchardCore.Settings;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 
--- INZFS/POCO/EqualityPoco.cs
+++ INZFS/POCO/EqualityPoco.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace INZFS.Web.POCO
+﻿namespace INZFS.Web.POCO
 {
     public class EqualityPoco
     {
